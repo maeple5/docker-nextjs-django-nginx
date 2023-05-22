e0edb44d32ddb21377991ec4f675e2153c273da1
maeple5 <maeplego@gmail.com>
2023-05-22 18:25:05 +0900

    update globals.css commit

--- frontend/web-front/src/app/page.tsx
+++ frontend/web-front/src/app/page.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
+'use client';
+import React, { FC, useEffect, useState } from 'react'
+import axios, { AxiosInstance } from 'axios'
 
+type Todo = {
+    id: string
+    title: String
+    body: String
+}
+ 
+export default function Hello() {
+    const [todos, setTodo] = useState<Todo[]>([])
 
-export default function Home() {
-  return (
-    <div>
-      <h1>新着記事</h1>
-      <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
-      </ul>
-    </div>
-  )
+    const getAPIData = async () => {
+        let instance: AxiosInstance
+
+        instance = axios.create({
+            baseURL: 'http://localhost:8080',
+        })
+
+        try {
+            const response = await instance.get('/api/')
+            console.log(response?.data)
+            const tododata = response?.data as Todo[]
+            setTodo(tododata)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <div>
+            hello world
+            <button onClick={() => getAPIData()}>click</button>
+            {todos.map((item) => (
+                <div key={item.id}>
+                    <h1>{item.title}</h1>
+                    <p>{item.body}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
